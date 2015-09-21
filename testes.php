62a573d51f30b300fb07f99185aaa57b01ef536a
Rafael Nery <rafael.nery@dbseller.com.br>
2015-09-21 12:55:21 -0300

    Modificaçõe de classes

---
+++ testes.php
@@ -0,0 +1,15 @@
+<?php 
+require 'vendor/autoload.php';
+
+
+$sSql               = "SELECT * FROM testes.teste inner join  coisa.neh on teste.codigo = neh.codigo where teste.xoxo = 1 and teste.xoxb is true;";
+$oObjeto            = new StdClass();
+$oObjeto->aArray    = array(1,2,3,4,5,"6", array(7));
+$oObjeto->backtrace = debug_backtrace();
+
+\PHP\Utils::dump_sql($sSql);
+\PHP\Utils::dump($sSql, $oObjeto);
+\PHP\Utils::kill($sSql, $oObjeto);
+echo "Não deve aparecer";
+
+
