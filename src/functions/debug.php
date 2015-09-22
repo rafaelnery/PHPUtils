7229ac2f6ed16701e122b759c66fec2b51ad712f
Rafael Nery <rafael.nery@dbseller.com.br>
2015-09-22 14:02:23 -0300

    Adicionada função kill_sql

--- src/functions/debug.php
+++ src/functions/debug.php
@@ -4,7 +4,12 @@ function kill() {
   return call_user_func_array("\\PHP\\Utils::kill", func_get_args());
 }
 
-
 function dump_sql($sSql) {
   return \PHP\Utils::dump_sql($sSql);
+}
+
+function kill_sql($sSql) {
+
+  \PHP\Utils::dump_sql($sSql);
+  die("FIM SQL");
 }
