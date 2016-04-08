87b25cfc30259b9ed408da934a315ebad0fd1286
Rafael Nery <rafael.nery@dbseller.com.br>
2016-04-08 15:34:46 -0300

    Added tests

---
+++ tests/ArrayDebuggerTest.php
@@ -0,0 +1,120 @@
+<?php 
+
+namespace PHP;
+
+class ArrayDebuggerTest extends \PHPUnit_Framework_TestCase 
+{
+
+  public $base = array(
+    'hum'  => 1, 
+    'dois' => 2, 
+    'tres' => 3
+  );
+
+  public function testGetter() {
+
+    $array = new ArrayDebugger($this->base);
+
+    $array->setLogger(function($retorno)  {
+      $this->assertEquals($retorno['type'], ArrayDebugger::TYPE_GET);
+    });
+
+    $teste = $array['hum'];
+
+    $array->setLogger(function($retorno) {
+      $this->assertNotEquals($retorno['type'], ArrayDebugger::TYPE_GET);
+    });
+
+    $array['quatro'] = 4;
+
+    /**
+     * offsetExists
+     */
+    isset($array['hum']);
+    /**
+     * offsetUnset
+     */
+    unset($array['quatro']);
+  }
+
+  public function testSetter() {
+
+    $array = new ArrayDebugger($this->base);
+
+    $array->setLogger(function($retorno)  {
+      $this->assertEquals($retorno['type'], ArrayDebugger::TYPE_SET);
+    });
+
+    $array['quatro'] = 4;
+
+    $array->setLogger(function($retorno) {
+      $this->assertNotEquals($retorno['type'], ArrayDebugger::TYPE_SET);
+    });
+
+    $teste = $array['hum'];
+
+    /**
+     * offsetExists
+     */
+    isset($array['hum']);
+    /**
+     * offsetUnset
+     */
+    unset($array['quatro']);
+  }
+
+  public function testIsset() {
+
+    $array = new ArrayDebugger($this->base);
+
+    $array->setLogger(function($retorno)  {
+      $this->assertEquals($retorno['type'], ArrayDebugger::TYPE_EXISTS);
+    });
+
+
+    /**
+     * offsetExists
+     */
+    isset($array['hum']);
+
+    $array->setLogger(function($retorno) {
+      $this->assertNotEquals($retorno['type'], ArrayDebugger::TYPE_EXISTS);
+    });
+
+    $array['quatro'] = 4;
+
+    $teste = $array['hum'];
+
+    /**
+     * offsetUnset
+     */
+    unset($array['quatro']);
+  }
+
+  public function testUnset() {
+
+    $array = new ArrayDebugger($this->base);
+
+    $array->setLogger(function($retorno)  {
+      $this->assertEquals($retorno['type'], ArrayDebugger::TYPE_UNSET);
+    });
+
+    /**
+     * offsetUnset
+     */
+    unset($array['dois']);
+
+    $array->setLogger(function($retorno) {
+      $this->assertNotEquals($retorno['type'], ArrayDebugger::TYPE_UNSET);
+    });
+    /**
+     * offsetExists
+     */
+    isset($array['hum']);
+
+    $array['quatro'] = 4;
+
+    $teste = $array['hum'];
+
+  }
+}
