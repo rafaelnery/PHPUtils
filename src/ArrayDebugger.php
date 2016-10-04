c7238ad5d0c06e137831adaa393190cbfac48c4b
Rafael Nery <rafanery0@gmail.com>
2016-10-04 09:10:56 -0300

    Update ArrayDebugger.php

--- src/ArrayDebugger.php
+++ src/ArrayDebugger.php
@@ -10,13 +10,14 @@ class ArrayDebugger extends \ArrayObject {
 
   private $logger;
 
-  public function __construct() {
+  public function __construct() 
+  {
 
-    $this->logger = function($data_log) {
-      return dump($data_log);
-    };
+      $this->logger = function($data_log) {
+        dump($data_log);
+      };
 
-    return call_user_func_array('parent::__construct', func_get_args()); 
+      call_user_func_array('parent::__construct', func_get_args()); 
   }
 
   /**
@@ -27,15 +28,16 @@ class ArrayDebugger extends \ArrayObject {
    * @param string $value
    * @param array  $backtrace
    */
-  public function sendLog($type, $key, $value = null, array $backtrace ) {
+  public function sendLog($type, $key, $value = null, array $backtrace ) 
+  {
 
-    $logger           = $this->logger;
-    $retorno['type']  = $type;  
-    $retorno['key']   = $key;
-    $retorno['value'] = $value;
-    $retorno['file']  = isset($backtrace[0]['file']) ? $backtrace[0]['file'] : '';
-    $retorno['line']  = isset($backtrace[0]['line']) ? $backtrace[0]['line'] : '';
-    return $logger($retorno);
+      $logger           = $this->logger;
+      $retorno['type']  = $type;  
+      $retorno['key']   = $key;
+      $retorno['value'] = $value;
+      $retorno['file']  = isset($backtrace[0]['file']) ? $backtrace[0]['file'] : '';
+      $retorno['line']  = isset($backtrace[0]['line']) ? $backtrace[0]['line'] : '';
+      return $logger($retorno);
   }
 
   /**
@@ -43,8 +45,9 @@ class ArrayDebugger extends \ArrayObject {
    *
    * @param \Closure $logger
    */
-  public function setLogger(\Closure $logger) {
-    $this->logger = $logger;
+  public function setLogger(\Closure $logger) 
+  {
+      $this->logger = $logger;
   }
 
   /**
@@ -52,10 +55,10 @@ class ArrayDebugger extends \ArrayObject {
    *
    * @param mixed $name
    */
-  public function offsetGet($name) { 
-
-    $this->sendLog(static::TYPE_GET, $name, null, debug_backtrace());
-    return call_user_func_array('parent::offsetGet', func_get_args()); 
+  public function offsetGet($name) 
+  { 
+      $this->sendLog(static::TYPE_GET, $name, null, debug_backtrace());
+      return call_user_func_array('parent::offsetGet', func_get_args()); 
   } 
 
   /**
@@ -64,10 +67,10 @@ class ArrayDebugger extends \ArrayObject {
    * @param mixed $name
    * @param mixed $value
    */
-  public function offsetSet($name, $value) { 
-
-    $this->sendLog(static::TYPE_SET, $name, $value, debug_backtrace());
-    return call_user_func_array('parent::offsetSet', func_get_args()); 
+  public function offsetSet($name, $value) 
+  { 
+      $this->sendLog(static::TYPE_SET, $name, $value, debug_backtrace());
+      return call_user_func_array('parent::offsetSet', func_get_args()); 
   } 
 
   /**
@@ -75,10 +78,10 @@ class ArrayDebugger extends \ArrayObject {
    *
    * @param mixed $name
    */
-  public function offsetExists($name) { 
-
-    $this->sendLog(static::TYPE_EXISTS, $name, null, debug_backtrace());
-    return call_user_func_array('parent::offsetExists', func_get_args()); 
+  public function offsetExists($name) 
+  { 
+      $this->sendLog(static::TYPE_EXISTS, $name, null, debug_backtrace());
+      return call_user_func_array('parent::offsetExists', func_get_args()); 
   } 
 
   /**
@@ -86,9 +89,9 @@ class ArrayDebugger extends \ArrayObject {
    *
    * @param mixed $name
    */
-  public function offsetUnset($name) { 
-
-    $this->sendLog(static::TYPE_UNSET, $name, null, debug_backtrace());
-    return call_user_func_array('parent::offsetUnset', func_get_args()); 
+  public function offsetUnset($name) 
+  { 
+      $this->sendLog(static::TYPE_UNSET, $name, null, debug_backtrace());
+      return call_user_func_array('parent::offsetUnset', func_get_args()); 
   } 
 } 
